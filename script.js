4d540429b826bc1b7567c46d1fafea3b884558bf
llallonen <lerushok@ural3000.local>
2022-07-19 19:58:07 +0500

    attempt to avoid render flicking (failed)

--- script.js
+++ script.js
@@ -12,49 +12,83 @@
 //     return siblings;
 // }
 
-// const bloko = document.querySelector('.bloko-v-spacing_base-7');
+// const vacancy = document.querySelector('.vacancy-title').parentNode;
 
+// const phraseItemChecker = vacancy.querySelector('.bloko-v-spacing_base-7');
+// const phraseItem = phraseItemChecker.nextSibling;
+// const counter = vacancy.querySelector('span[class*="count--"]');
+// console.log(phraseItemChecker, phraseItem, counter)
 
-const DEL_SELECTORS = '.bloko-v-spacing_base-7';
+const DEL_SELECTOR = '.bloko-v-spacing_base-7';
 
 const mo = new MutationObserver(onMutation);
 
-onMutation([{ addedNodes: [document.documentElement] }]);
+onMutation([{addedNodes: [document.documentElement]}]);
 observe();
 
 function onMutation(mutations) {
-    let stopped;
-    for (const { addedNodes } of mutations) {
-        for (const n of addedNodes) {
-            //без проверки на tagName почему-то не работает
-            if (n.tagName) {
-                if (n.matches(DEL_SELECTORS)) {
-                    stopped = true;
-                    mo.disconnect();
-                    n.remove();
-                } else if (n.firstElementChild && n.querySelector(DEL_SELECTORS)) {
-                    stopped = true;
-                    mo.disconnect();
-                    for (const el of n.querySelectorAll(DEL_SELECTORS)) el.remove();
-                }
-            }
-        }
+  let stopped;
+  for (const {addedNodes} of mutations) {
+    for (const n of addedNodes) {
+      if (n.tagName) {
+        if (n.matches(DEL_SELECTOR)) {
+          stopped = true;
+          
+
+          const del = n.nextSibling;
+          const del2 = del.nextSibling;
+          const del3 = del2.nextSibling;
+          const del4 = del3.nextSibling;
+          const delArr = [del, del2, del3, del4];
+          delArr.map(item =>  item.remove())
+          mo.disconnect();
+        } 
+      }
     }
-    if (stopped) observe();
+  }
+  if (stopped) observe();
 }
 
 function observe() {
-    mo.observe(document, {
-        subtree: true,
-        childList: true,
-    });
+  mo.observe(document, {
+    subtree: true,
+    childList: true,
+  });
 }
 
-// try {
-//     const bloko = document.querySelector(".bloko-v-spacing_base-7");
+// const mo = new MutationObserver(onMutation);
+// onMutation([{addedNodes: [document.documentElement]}]);
+// observe();
 
-//     const embarassingPhrase = getSiblings(bloko);
-//     embarassingPhrase.map(item => item.remove());
-// } catch {
-//     console.log('error!!!!')
+// function onMutation(mutations) {
+//   let stopped;
+//   for (const {addedNodes} of mutations) {
+//     for (const n of addedNodes) {
+//       if (n.nodeName) {
+//         if (n.matches(DEL_SELECTOR)) {
+//           stopped = true;
+//           mo.disconnect();
+
+//           let phraseItem2 = phraseItem.nextSibling;
+//           phraseItem.remove();
+//           phraseItem2.remove();
+//           counter.remove();
+
+//         }
+//         // } else if (n.firstElementChild && n.querySelector(DEL_SELECTOR)) {
+//         //   stopped = true;
+//         //   mo.disconnect();
+//         //   for (const el of n.querySelectorAll(DEL_SELECTOR)) el.remove();
+//         // }
+//       }
+//     }
+//   }
+//   if (stopped) observe();
+// }
+
+// function observe() {
+//   mo.observe(document, {
+//     subtree: true,
+//     childList: true,
+//   });
 // }
